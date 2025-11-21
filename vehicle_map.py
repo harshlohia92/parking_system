a3b3a0655f67a7eb53fbc52d0b707300530c7dd6
harshlohia92 <lohiaharsh00@gmail.com>
2025-11-21 13:10:06 +0530

    Add files via upload

---
+++ vehicle_map.py
@@ -0,0 +1,28 @@
+
+VEHICLE_CLASSES = {
+    0: "bus",
+    1: "family_sedan",
+    2: "suv",
+    3: "jeep",
+    4: "mini_bus",
+    5: "heavy_truck",
+    6: "fire_engine",
+    7: "taxi",
+    8: "truck",
+    9: "racing_car"
+}
+
+
+VEHICLE_TO_SLOT = {
+    "suv": "large",
+    "family_sedan": "medium",
+    "jeep": "medium",
+    "bus": "xl",
+    "mini_bus": "large",
+    "heavy_truck": "xl",
+    "truck": "xl",
+    "taxi": "medium",
+    "racing_car": "medium",
+
+    "default": "medium"
+}
